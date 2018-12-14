f80c0aa5fd26dd85c604ddba932a6e0fb1437d0c
unknown <hubertm33@wp.pl>
2018-12-14 18:05:44 +0100

    Wersja statyczna ok.

---
+++ src/main/java/WypozyczenieAuta.java
@@ -0,0 +1,16 @@
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Wypozyczenie Auta")
+public class WypozyczenieAuta extends ServletProcessApplication {
+
+//    Map<String, String> productTypes = new HashMap<String, String>();
+//productTypes.put("001", "Notebook");
+//productTypes.put("002", "Server");
+//productTypes.put("003", "Workstation");
+//
+//execution.setVariable("AVAILABLE_PRODUCT_TYPES",
+//    objectValue(productTypes)
+//    .serializationDataFormat(SerializationDataFormats.JSON)
+//    .create());
+}
